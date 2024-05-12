91d52a3642696bd366fe26ba644c9c8e913305a6
Yuri Vasconcelos <161763328+YuriVascsH@users.noreply.github.com>
2024-05-12 15:30:08 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,34 @@
+// Codigo novo
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router/router.js";
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6MZUUXYJT2eG3lKwk386nSOXg4vOctNk",
+  authDomain: "projetofirebase-f5ab1.firebaseapp.com",
+  projectId: "projetofirebase-f5ab1",
+  storageBucket: "projetofirebase-f5ab1.appspot.com",
+  messagingSenderId: "857335660599",
+  appId: "1:857335660599:web:5d19e0192348e27d9b3fb2",
+  measurementId: "G-12B6E7WGDR",
+};
+
+initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+
+new Vue({
+  vuetify,
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
+//fim codigo novo
+
+
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
