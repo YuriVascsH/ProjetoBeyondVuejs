559107cab56e478dd0789824e1b4907175662468
Yuri Vasconcelos <161763328+YuriVascsH@users.noreply.github.com>
2024-05-12 23:43:04 -0300

    Login

--- src/pages/pop.vue
+++ src/pages/pop.vue
@@ -28,3 +28,51 @@
     </v-dialog>
   </v-app>
 </template> -->
+
+
+<!-- Parte referente ao login -->
+<template>
+  <v-app>
+    <h1>Sign In to an Account</h1>
+    <p><input type="text" placeholder="Email" v-model="email" /></p>
+    <p><input type="password" placeholder="Password" v-model="password" /></p>
+    <p><button @click="register">Submit</button></p>
+    <p><button @click="signInWithGoogle">Sign In with Google</button></p>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import router from "@/router/router";
+const email = ref("");
+const errMsg = ref();
+const password = ref("");
+const register = () => {
+  createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then(() => {
+      console.log("Registro sucesso");
+      router.push("/calender");
+    })
+    .catch((error) => {
+      console.log(error.code);
+      switch (error.code) {
+        case "auth/invalid-email":
+          errMsg.value = "Email inválido";
+          break;
+        case "auth/user-not-found":
+          errMsg.value = "Nenhuma conta com esse email";
+          break;
+        case "auth/wrong-password":
+          errMsg.value = "Senha incorreta";
+          break;
+        default:
+          errMsg.value = "Email or senha incorreta";
+          break;
+      }
+    });
+};
+
+const signInWithGoogle = () => {};
+</script>
+<!-- Fim do novo códgio -->
